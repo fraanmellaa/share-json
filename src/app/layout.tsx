f985d17a6417cada03be67e67fb36fcc4407568c
Fran Mella <franpola14@gmail.com>
2025-03-01 18:30:31 +0100

    feat: added made by and link to my website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { roboto } from "./ui/fonts";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: {
@@ -24,6 +25,32 @@ export default function RootLayout({
           <div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.05)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.05)_1px,transparent_1px)] bg-[size:32px_32px]"></div>
         </div>
         {children}
+
+        <span className="absolute bottom-2 left-0 right-0 mx-auto text-white text-sm flex items-center justify-center">
+          Made with 💙 by
+          <Link
+            href="https://franmella.dev/en"
+            rel="noopener noreferer"
+            target="_blank"
+            className="flex items-center justify-center space-x-1 ml-2 hover:text-white/80 transition-all duration-300"
+          >
+            <span>Fran Mella</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+            >
+              <g fill="none" stroke="currentColor" strokeWidth="1">
+                <path d="M15 3.5h5.5m0 0V9m0-5.5l-8 8" />
+                <path
+                  strokeLinecap="round"
+                  d="M11.5 5.5h-4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-4"
+                />
+              </g>
+            </svg>
+          </Link>
+        </span>
       </body>
     </html>
   );
