821da93d09f67507cae29bd9ccc362f726d1e7f5
Fran Mella <franpola14@gmail.com>
2025-03-05 13:42:52 +0100

    redeploy

--- src/app/(app)/view/[json_id]/not-found.tsx
+++ src/app/(app)/view/[json_id]/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <main className="flex h-screen overflow-hidden flex-col items-center justify-center gap-2">
       <h2 className="text-xl font-semibold">404 Not Found</h2>
-      <p>Could not find the requested invoice.</p>
+      <p>Could not find the requested JSON.</p>
       <Link
         href="/"
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
